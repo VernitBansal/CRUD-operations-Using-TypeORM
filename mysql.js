304df25332cd96fd153b0c77c9f95507e43c226b
Vernit Bansal <113528172+VernitBansal@users.noreply.github.com>
2024-01-21 18:10:44 +0530

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ const path = require("path");
 const dataSource = new typeorm.DataSource({
     type: "mysql",
     host: "localhost",
-    port: 3306,
+    port: ....,
     username: "....",
     password: "....",
     database: "....",
